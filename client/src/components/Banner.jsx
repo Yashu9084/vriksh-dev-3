77813a614159da49f0b0cf209b61e752eedbd448
Yashu9084 <yashumittal9084@gmail.com>
2024-06-02 07:21:07 +0530

    Banner section done

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="marquee-container my-24">
+      <div className="marquee-container">
+        <div className="marquee-content bg-transparent font-neue  flex items-center">
+          <span className="bg-transparent text-[80px]">Creating your success story      </span>
+          <span className="bg-transparent text-[80px]">Creating your success story      </span>
+          <span className="bg-transparent text-[80px]">Creating your success story      </span>
+          <span className="bg-transparent text-[80px]">Creating your success story      </span>
+          <span className="bg-transparent text-[80px]">Creating your success story      </span>
+          <span className="bg-transparent text-[80px]">Creating your success story      </span>
+          <span className="bg-transparent text-[80px]">Creating your success story      </span>
+          <span className="bg-transparent text-[80px]">Creating your success story      </span>
+          <span className="bg-transparent text-[80px]">Creating your success story      </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
